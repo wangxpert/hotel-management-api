81ddca8c213737f5200618f67606a8f46f1d3bbc
“Zootopia106” <pro.zootopia106@gmail.com>
2016-11-23 23:12:16 +0100

    init commit

---
+++ App/Models/BookingModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const BookingModel = new mongoose.Schema({
+    checkin: {
+        type: Date,
+        required: true
+    },
+
+    checkout: {
+        type: Date,
+        required: true
+    },
+
+    rooms: [{ 
+        roomId: {type: String, required: true} 
+    }]
+});
+
+module.exports = mongoose.model('booking', BookingSchema);
---
+++ App/Models/HotelModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const HotelModel = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+
+    city: {
+        type: String,
+        required: true
+    },
+
+    createdAt: {
+        type: Date
+    },
+
+    rooms: [
+        {            
+            roomNumber: {type: Number, required: true},
+            roomType: {type: String, required: true},
+            price: {type: String, required: true},
+            available: {type: Number, required: true, default: 1}   
+        }
+    ]
+});
+
+module.exports = mongoose.model('hotel', HotelModel);
